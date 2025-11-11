7ceeed7b0f0953b4724cfcb5bda82def33a3a864
luiskevinms <luiskevinms@gmail.com>
2025-11-11 10:11:04 -0700

    Se agregaron imagenes de los cheems y modificaciones en el index.html

---
+++ static/js/cheems.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", () => { 
+
+    const randomNumber = Math.floor(Math.random() *14) + 1;
+
+    // TODO: Eliminar antes de publicar el juego
+    console.debug("Numero random:" + randomNumber);
+
+    const imagenes = document.querySelectorAll(".cheems-card img");
+
+    imagenes.forEach((img, index) => {
+        const id = index +1;
+        img.dataset.id = index +1;
+
+        img.addEventListener("click", () => {
+            if (id==randomNumber) {
+                img.src = window.IMG_BAD;
+                //alert("Perdiste")
+            } else {
+                img.src = window.IMG_OK;
+                //alert("Ganaste")
+            }
+        });
+
+    })
+
+
+});
